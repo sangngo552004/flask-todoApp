7575028c983ff13e989a817747248036dc5fbefa
sangngo552004 <sangngo552004@gmail.com>
2024-11-12 15:11:37 +0700

    code python in HTML

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return "Hello world"
+@app.route('/user/<name>')
+def hello_world(name):
+    return render_template('index.html', content = name, cars = ["BMW","Mercedes","Vinfast", "Tesla"])
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
