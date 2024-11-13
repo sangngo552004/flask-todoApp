ca87048a0c028b3908acb39b6e551eb0b93b2802
sangngo552004 <sangngo552004@gmail.com>
2024-11-13 14:33:01 +0700

    config environment variable

--- todoList/__init__.py
+++ todoList/__init__.py
@@ -1,5 +1,14 @@
 from flask import Flask
+import os
+from dotenv import load_dotenv
+
+# Biến môi trường
+load_dotenv()
+SECRET_KEY = os.environ.get("SECRET_KEY")
+DB_NAME = os.environ.get("DB_NAME")
 
 def create_app():
-    return Flask(__name__)
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = SECRET_KEY
+    return app
 
